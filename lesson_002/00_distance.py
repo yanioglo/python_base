505fd1940d61d07d063411a13b626aac9903a691
Tester from work <yanioglo@mail.ru>
2024-02-29 10:28:49 +0300

    Задание № 00 исправил название словаря с городами

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -5,7 +5,7 @@
 
 from pprint import pprint
 
-sites = {
+cities = {
     'Moscow': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
@@ -16,9 +16,9 @@ sites = {
 
 distances = dict ()
 
-moscow = sites['Moscow']
-london = sites['London']
-paris = sites['Paris']
+moscow = cities['Moscow']
+london = cities['London']
+paris = cities['Paris']
 
 moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
 moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
